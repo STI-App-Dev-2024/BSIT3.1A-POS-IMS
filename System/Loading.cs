69ea8897a607daca6ae8a4c5659c1dca3c6f433f
Rehneil Lor <151996583+reinlor@users.noreply.github.com>
2024-10-10 01:25:39 +0800

    Add files via upload

---
+++ System/Loading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           Opacity += .2;
+        }
+
+        private void Loading_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value += 1;
+                progPercent.Text = progressBar1.Value.ToString() + "%";
+            }
+
+            else {
+                if(progressBar1.Value == 100) 
+                {
+                    timer2.Stop();
+
+                    Login log = new Login();
+                    log.Show();
+                    this.Hide();
+                }
+            }
+        }
+    }
+}
---
+++ System/fitDB.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg;
+
+namespace System
+{
+    public class fitDB
+    {
+        MySql.Data.MySqlClient.MySqlConnection con;
+        string myConnectionString;
+        static string host = "localhost";
+        static string database = "fitwhitedb";
+        static string userDB = "root";
+        public static string fitbase = "server=" + host + ";database=" + database + ";user=" + userDB + ";Password=";
+
+        public bool Open()
+        {
+            try
+            {
+                fitbase = "server=" + host + ";database=" + database + ";user=" + userDB + ";Password=";
+                con = new MySqlConnection(fitbase);
+                con.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+
+        public void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+
+        public DataSet ExecuteDataSet(string sql)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, con);
+                da.Fill(ds, "result");
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+
+        public MySqlDataReader ExecuteReader(string sql)
+        {
+            try
+            {
+                MySqlDataReader reader;
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                reader = cmd.ExecuteReader();
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+
+        public int ExecuteNonQuery(string sql)
+        {
+            try
+            {
+                int affected;
+                MySqlTransaction mytransaction = con.BeginTransaction();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = sql;
+                affected = cmd.ExecuteNonQuery();
+                mytransaction.Commit();
+                return affected;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return -1;
+        }
+
+    }
+}
